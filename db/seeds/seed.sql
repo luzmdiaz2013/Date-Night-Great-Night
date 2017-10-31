a3cfadf551f58f7f59823058b3c36d69592b8e08
Luz Diaz <luzmdiaz2013@gmail.com>
2017-10-30 22:22:55 -0400

    server.js done

---
+++ db/seeds/seed.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO datestable (date, time, location, description, url) VALUES
+('Tuesday 10/31/17', '9:00pm', 'West Village', 'Halloween Parade NYC', 'http://halloween-nyc.com/'),
+('Saturday 11/04/17','2:30pm', 'Hudson Valley', 'Jack-O-Lantern display', 'http://www.hudsonvalley.org/events/blaze?utm_source=Google&utm_medium=Search&utm_content=General&utm_campaign=Blaze%2017&gclid=Cj0KCQjwm9vPBRCQARIsABAIQYeSqoVRLRa3I21H4oGEF7-4cyVPw2tCysmjuX09_-9UYPRtLHHMJ8gaAiIDEALw_wcB'),
+('Wednesday 11/15/17', '6:30pm', 'Williamsburg, BK', 'MOFAD', 'http://www.mofad.org/')
+
--- server.js
+++ server.js
@@ -1,23 +1,20 @@
-// const express = require('express');
-// const logger = require('morgan');
-// const path = require('path');
-// const bodyParser = require('body-parser');
-// const methodOverride = require('method-override');
-// const cookieParser = require('cookie-parser');
-// const session = require('express-session');
-// const passport = require('passport');
-
-// const app = express();
-// require('dotenv').config();
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`ALL GOOD on port ${PORT}`)
-// });
 
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
+const bodyParser = require();
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`ALL GOOD on port ${PORT}`)
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -26,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(methodOverride('_method'));
+app.use(methodOverride('-method'));
 app.use(cookieParser());
 app.use(session({
   secret: process.env.SECRET_KEY,
@@ -42,13 +39,28 @@ app.get('/', (req, res) => {
   });
 });
 
+// const authRoutes = require('./routes/auth-routes');
+// app.use('/auth', authRoutes);
+// const userRoutes = require('./routes/user-routes');
+// app.use('/user', userRoutes);
+// const todoRoutes = require('./routes/todo-routes');
+// app.use('/todos', todoRoutes);
+
+// app.use('*', (req, res) => {
+//   res.status(400).send('Not Found');
+// });
+
 const authRoutes = require('./routes/auth-routes');
 app.use('/auth', authRoutes);
 const userRoutes = require('./routes/user-routes');
 app.use('/user', userRoutes);
-const todoRoutes = require('./routes/todo-routes');
-app.use('/todos', todoRoutes);
+const dateNightRoutes = require('./routes/dateNight-routes');
+app.use('/dateNights', dateNightRoutes);
 
 app.use('*', (req, res) => {
   res.status(400).send('Not Found');
 });
+
+
+
+
