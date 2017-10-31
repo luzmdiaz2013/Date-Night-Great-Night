7fdf16773a5dbd3e8b4bca809c99aa2da2d14c0e
Luz Diaz <luzmdiaz2013@gmail.com>
2017-10-30 23:31:22 -0400

    config.js complete

---
+++ db/config.js
@@ -0,0 +1,24 @@
+
+const options = {
+  query: (e) => {
+    console.log(e.query);
+  },
+};
+
+const pgp = require('pg-promise')(options);
+
+function setb() {
+  if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+    return pgp({
+      database: 'datestable',
+      port: 5432,
+      host: 'localhost',
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    return pgp(process.env.DATABASE_URL);
+  }
+}
+
+const db =setDb;
+
+module.exports = db;
